3f8994aca8accd9227ecee8a4a2e294fa3eea0db
Prajjwall123 <pokhrelprajwal29@gmail.com>
2025-03-20 09:25:08 +0545

    course model

--- growth_mate_app/models.py
+++ growth_mate_app/models.py
@@ -30,3 +30,34 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} - {self.role}"
+
+
+class Course(models.Model):
+    image = models.ImageField(upload_to='static/course_images/', blank=True, default='static/assets/images/communication.png')  
+    title = models.CharField(max_length=255)
+    duration = models.CharField(max_length=50) 
+    due_date = models.DateField()
+    about_this_course = models.TextField()
+    uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE) 
+
+    def __str__(self):
+        return self.title
+
+
+class CourseContent(models.Model):
+    course = models.ForeignKey(Course, related_name="contents", on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.course.title} - {self.title}"
+
+
+class Section(models.Model):
+    course_content = models.ForeignKey(CourseContent, related_name="sections", on_delete=models.CASCADE)
+    heading = models.CharField(max_length=255) 
+    description = models.TextField(blank=True, null=True)
+    videos = models.FileField(upload_to='static/course_media/videos/', blank=True, null=True)  
+    images = models.ImageField(upload_to='static/course_media/images/', blank=True, null=True) 
+
+    def __str__(self):
+        return self.heading
